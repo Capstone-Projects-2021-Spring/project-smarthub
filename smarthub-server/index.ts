05fdb7d5f8d8145baaeb08efaef578a349f0f404
johnnygangoo <59904666+johnnygangoo@users.noreply.github.com>
2021-03-08 12:04:42 -0500

    Added create S3 folder endpoint

--- smarthub-server/index.ts
+++ smarthub-server/index.ts
@@ -1,14 +1,31 @@
-import express from 'express';
+import express from 'express' ;
 import {spawn} from 'child_process';
 import * as socketio from 'socket.io';
 import puppeteer from 'puppeteer';
 import { VideoController } from './VideoController';
 import path from 'path';
 import http = require("http");
+import bodyParser from 'body-parser';
+const createFolder = require('./public/aws/amazon_s3').createFolder
 
 const app = express();
 // Express built-in middleware function static allows serving static files.
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//Ensure incoming data has json format
+app.use(bodyParser.json());
+
+//Allows any address to interact with our server
+app.use(function (req : any, res : any, next : any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Create a http server object from the express application.
 const httpServer = http.createServer(app);
 // Create new videoController instance.
@@ -27,6 +44,15 @@ app.get("/stopStream", async (req, res) => {
 app.get("/startStream", (req, res) => {
   run();
 });
+
+//-----------------------------------------s3---------------------------------------
+
+//Below will create two folders a users folder (account) and sub folder(s) (profiles)
+app.post("/createS3Folder", (req : any, res : any) => {
+    createFolder(req.body.userName, req.body.profileName);  
+})
+
+//--------------------------------------------------------------------------------
 
 async function run () {
     browser = await puppeteer.launch({executablePath: 'chromium-browser', headless: true, args: ['--use-fake-ui-for-media-stream', '--mute-audio'] });
