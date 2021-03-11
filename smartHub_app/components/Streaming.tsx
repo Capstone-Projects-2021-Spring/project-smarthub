613258a7bddfe2ea4d44d42f7c6371e141abff36
Johnny Gangoo <tuh34169@temple.edu>
2021-03-11 13:22:27 -0500

    Updated streaming route

--- smartHub_app/components/Streaming.tsx
+++ smartHub_app/components/Streaming.tsx
@@ -18,7 +18,7 @@ export default class Streaming extends Component<{route: any, navigation: any}, 
 
     beginStream = () => {
         if(this.state.responseText!== 'Stream Starting.'){
-            axios.get('http://100.19.94.49:4000/startStream').then((response) => {
+            axios.get('http://100.19.94.49:4000/video/startStream').then((response) => {
                     Toast.show({
                         type: 'success',
                         text1: 'Processing Request Please Wait...',
@@ -50,7 +50,7 @@ export default class Streaming extends Component<{route: any, navigation: any}, 
     
     stopStream = () => {
         if(this.state.responseText !== 'Stream Closing'){
-            axios.get('http://100.19.94.49:4000/stopStream').then((response) => {
+            axios.get('http://100.19.94.49:4000/video/stopStream').then((response) => {
                 this.setState({responseText: response.data})
                 Toast.show({
                     type: 'error',
@@ -123,7 +123,7 @@ export default class Streaming extends Component<{route: any, navigation: any}, 
                     flex: 1,
                 }}
                 originWhitelist={['*']}
-                source={{html: '<iframe style="box-sizing: border-box; width: 100%; height: 99%; border: 15px solid #FF9900; background-color: #222222"; src="http://100.19.94.49:4000/watch.html" frameborder="0" allow="autoplay encrypted-media" allowfullscreen></iframe>'}} 
+                source={{html: '<iframe style="box-sizing: border-box; width: 100%; height: 99%; border: 15px solid #FF9900; background-color: #222222"; src="http://100.19.94.49:4000/video/watch.html" frameborder="0" allow="autoplay encrypted-media" allowfullscreen></iframe>'}} 
                 mediaPlaybackRequiresUserAction={false}
                 />
             <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 50, paddingBottom: 80}}>
