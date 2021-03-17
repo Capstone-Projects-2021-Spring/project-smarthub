61b477460c826bbf5fd604812f3f780d3e42b0d5
Johnny Gangoo <tuh34169@temple.edu>
2021-03-17 00:41:58 -0400

    Added color picker for lights

--- smartHub_app/components/pages/SmartLightsPage.tsx
+++ smartHub_app/components/pages/SmartLightsPage.tsx
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text, TouchableOpacity, Dimensions} from 'react-native';
+import { ColorPicker } from 'react-native-color-picker'
+import hexRgb from 'hex-rgb'
+import axios from 'axios';
 
 
 export default class SmartLight extends Component{
+
+    changeLightSingleColor(obj: any){
+        obj.brightness = 250;
+        obj.randomize = false;
+        console.log(obj);
+        axios.post("http://johnnyspi.ddns.net:8000/lights", obj) .then((response) => {
+            console.log(response.data)
+        }, (error) => {
+            console.log(error);
+        })
+    }
+
+
     render(){
         return(
-            <Text>Need to implement light feature</Text>
+            <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <ColorPicker
+            onColorSelected={(color:any) => (this.changeLightSingleColor(hexRgb(color)))}
+            hideSliders={false}
+            style={{width: "90%", height: "50%"}}
+          />
+          </View>
         )
     }
 }
