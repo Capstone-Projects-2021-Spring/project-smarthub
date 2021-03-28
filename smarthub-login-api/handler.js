33b9b8486d71c99acb0023d370eeb12541c20ec1
Johnny Gangoo <tuh34169@temple.edu>
2021-03-27 21:33:30 -0400

    Updated folder directories and aws routes

--- smarthub-login-api/handler.js
+++
@@ -1,9 +0,0 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./src/index');
-
-const server = awsServerlessExpress.createServer(app);
-
-exports.users = (event, context,) => {
-	return awsServerlessExpress.proxy(server, event, context);
-}
-
